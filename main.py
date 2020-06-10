3cad5731fb4658562ec2da34c03b12831095bca0
mixelburg <52622705+mixelburg@users.noreply.github.com>
2020-06-11 00:33:06 +0300

    nh

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def get_input():
     while not check_input(choice):
         try:
             choice = int(input("Enter your choice: "))
-        except Exception as e:
+        except:
             print(f"{RED}[!] Only numbers allowed!{RESET}")
 
     return choice
